1d3e78736d2b20e5743b4b631d1b680330f3df2f
BattleNest <battlenestofficial@gmail.com>
2026-02-13 21:21:15 +0530

    Update tournament-detail.tsx

--- client/src/pages/tournament-detail.tsx
+++ client/src/pages/tournament-detail.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useQuery, useMutation } from "@tanstack/react-query";
 import { useParams, useLocation } from "wouter";
+import { useEffect } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
