9b3667ae6e52d5768c608f9992fd27781d50fde6
BattleNest <battlenestofficial@gmail.com>
2026-02-14 10:42:35 +0530

    Update home.tsx

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -520,7 +520,7 @@ const fadeUp = {
               </Card>
             ))}
           </div>
-        ) : upcomingTournaments.length > 0 ? (
+        ) : liveTournaments.length > 0 ? (
           <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
 
             {upcomingTournaments.map((t, index) => {
